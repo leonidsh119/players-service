534b5c83f46d3b1971cc49062f878d42a156d359
Leon <28947655+leonidsh119@users.noreply.github.com>
2023-10-05 19:56:34 +0300

    swagger fixed

---
+++ players-service-contract/src/main/java/org/example/players/sdk/Player.java
@@ -0,0 +1,28 @@
+package org.example.players.sdk;
+
+public class Player {
+    private String playerID;
+    private String birthYear;
+    private String birthMonth;
+    private String birthDay;
+    private String birthCountry;
+    private String birthState;
+    private String birthCity;
+    private String deathYear;
+    private String deathMonth;
+    private String deathDay;
+    private String deathCountry;
+    private String deathState;
+    private String deathCity;
+    private String nameFirst;
+    private String nameLast;
+    private String nameGiven;
+    private String weight;
+    private String height;
+    private String bats;
+    private String throwz;
+    private String debut;
+    private String finalGame;
+    private String retroID;
+    private String bbrefID;
+}
