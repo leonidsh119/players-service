51103f15e8995f0ab9da4970ade0fce6ba3a65a1
Leon <28947655+leonidsh119@users.noreply.github.com>
2023-10-05 20:07:28 +0300

    swagger fixed

--- players-service-server/src/main/java/org/example/players/server/controller/PlayersController.java
+++ players-service-server/src/main/java/org/example/players/server/controller/PlayersController.java
@@ -1,12 +1,16 @@
 package org.example.players.server.controller;
 
 import org.example.players.sdk.FindUnreachableNodesRequest;
+import org.example.players.sdk.Player;
 import org.example.players.server.service.PlayersService;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/players")
@@ -22,5 +26,17 @@ public class PlayersController extends BaseController {
         _logger.trace("Searching for unreachable nodes in graph [{}] from node [{}]", request.getGraph(), request.getE1());
         Iterable<String> unreachableNodes = _service.findUnreachableNodes(request.getE1(), request.getGraph());
         return String.format("{%s}", String.join(",", unreachableNodes));
+    }
+
+    @GetMapping
+    @Schema
+    List<Player> listPlayers() {
+        return new ArrayList<Player>();
+    }
+
+    @GetMapping("/{playerID}")
+    @Schema
+    Player getPlayer(@RequestParam("playerID") String playerId) {
+        return new Player();
     }
 }
